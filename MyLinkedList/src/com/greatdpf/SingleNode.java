5b25e3510649c693bc1063b6f7b011c7d0101660
dpf <greatdpf@163.com>
2020-10-24 12:13:10 +0800

    创建双列表节点

---
+++ MyLinkedList/src/com/greatdpf/SingleNode.java
@@ -0,0 +1,17 @@
+package com.greatdpf;
+
+/**
+ * 简单的单链表的节点
+ *
+ * @author : dpf
+ * @version : V1.0
+ * @date : 2020/10/24
+ */
+public class SingleNode {
+    private SingleNode first;
+    private int data;
+
+    public SingleNode(int data) {
+        this.data = data;
+}
+}
