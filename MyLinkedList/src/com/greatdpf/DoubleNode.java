bf2527a54af8dcf2a248c9672649c80d45de4aad
dpf <greatdpf@163.com>
2020-10-24 12:12:54 +0800

    创建单链表节点

---
+++ MyLinkedList/src/com/greatdpf/DoubleNode.java
@@ -0,0 +1,14 @@
+package com.greatdpf;
+
+/**
+ * 双链表的节点
+ *
+ * @author : dpf
+ * @version : V1.0
+ * @date : 2020/10/24
+ */
+public class DoubleNode {
+    public DoubleNode first;
+    public DoubleNode last;
+
+}
