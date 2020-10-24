4758f586828bb99adcd9c8af77c173566927bcfd
dpf <greatdpf@163.com>
2020-10-24 13:41:14 +0800

    晚上双链表节点

--- MyLinkedList/src/com/greatdpf/DoubleNode.java
+++ MyLinkedList/src/com/greatdpf/DoubleNode.java
@@ -8,7 +8,47 @@ package com.greatdpf;
  * @date : 2020/10/24
  */
 public class DoubleNode {
-    public DoubleNode first;
-    public DoubleNode last;
+    /**
+     * 存放上一个节点
+     */
+    private DoubleNode previous;
+    /**
+     * 存放下一个节点
+     */
+    private DoubleNode next;
+    /**
+     * 存放数据
+     */
+    private int data;
 
+    public DoubleNode() {
+    }
+
+    public DoubleNode(int data) {
+        this.data = data;
+    }
+
+    public DoubleNode getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(DoubleNode previous) {
+        this.previous = previous;
+    }
+
+    public DoubleNode getNext() {
+        return next;
+    }
+
+    public void setNext(DoubleNode next) {
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
 }
