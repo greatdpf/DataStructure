5fcfb88c9e65b8cf6d5ac5fd38d29488f4fbaa97
dpf <greatdpf@163.com>
2020-10-24 13:09:19 +0800

    完善单链表节点

--- MyLinkedList/src/com/greatdpf/SingleNode.java
+++ MyLinkedList/src/com/greatdpf/SingleNode.java
@@ -8,10 +8,26 @@ package com.greatdpf;
  * @date : 2020/10/24
  */
 public class SingleNode {
-    private SingleNode first;
+    private SingleNode next;
     private int data;
+
+    public SingleNode getNext() {
+        return next;
+    }
+
+    public void setNext(SingleNode next) {
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
 
     public SingleNode(int data) {
         this.data = data;
-}
+    }
 }
